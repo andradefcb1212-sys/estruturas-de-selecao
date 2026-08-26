75f205ee9a1cd2750446abb2d34f4bb5e7d04928
Thiago Andrade <andradefcb1212@gmail.com>
2026-08-25 20:26:52 -0400

    Sexto Commit

---
+++ ex24.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main(){
+    int n1, n2, n3;
+    
+    printf("Insira três números: ");
+    scanf("%d %d %d\n", &n1, &n2, &n3);
+
+    printf("-------------------\n");
+
+    if (n1 > n2 && n1 > n3){
+        if (n2 > n3){
+            printf("%d %d %d", n1, n2, n3);
+        } else {
+            printf("%d %d %d", n1, n3, n2);
+        }
+    } else if (n2 > n1 && n2 > n3) {
+        if (n1 > n3) {
+            printf("%d %d %d", n2, n1, n3);
+        } else {
+            printf("%d %d %d", n2, n3, n1);
+        }
+    } else if (n3 > n2 && n3 > n1) {
+        if (n2 > n1) {
+            printf("%d %d %d", n3, n2, n1);
+        } else {
+            printf("%d %d %d", n3, n1, n2);
+        }
+    }
+    return 0;
+}
